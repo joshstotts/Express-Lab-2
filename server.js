e8522ea045ce85cd3662d99d93b4b188dfd0f0a5
joshstotts <94932808+joshstotts@users.noreply.github.com>
2021-12-20 22:38:25 -0600

    Tip calc and Magic 8 ball complete

--- server.js
+++ server.js
@@ -11,7 +11,43 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`Hello there ${req.params.name}`);
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    console.log(req.params);
+    let tip = Math.abs(req.params.tipPercentage / req.params.total) * 100;
+    res.send(`${tip}`);
+});
 
+
+var phrase = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good", 
+    "Yes", 
+    "Signs point to yes", 
+    "Reply hazy try again", 
+    "Ask again later", 
+    "Better not tell you now", 
+    "Cannot predict now", 
+    "Concentrate and ask again", 
+    "Don't count on it", 
+    "My reply is no", 
+    "My sources say no", 
+    "Outlook not so good", 
+    "Very doubtful"
+];
+
+let answer = phrase[Math.floor(Math.random() * phrase.length)]
+console.log(answer);
+
+app.get('/magic/:question', (req, res) => {
+    console.log(req.params);
+    res.send(`<h1>${req.params.question}:</h1> ${answer}`);
+});
 
 
 app.listen(3000, () => {
